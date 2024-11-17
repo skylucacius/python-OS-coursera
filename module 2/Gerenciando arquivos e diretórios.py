0cb61529b5e36984219c3e704a8a97b077ccfd14
skylucacius <skylucacius@hotmail.com>
2024-11-16 22:05:20 -0300

    módulo 2 - em andamento

--- module 2/Gerenciando arquivos e diretórios.py
+++ module 2/Gerenciando arquivos e diretórios.py
@@ -56,13 +56,13 @@
 # Pergunta 5
 # A função parent_directory retorna o nome do diretório que está localizado logo acima do diretório de trabalho atual. Lembre-se de que "..." é um alias de caminho relativo que significa "ir até o diretório pai". Preencha as lacunas para concluir essa função.
 
-import os
-def parent_directory():
-  # Create a relative path to the parent 
-  # of the current working directory 
-  relative_parent = os.path.join(os.getcwd(), "..")
+# import os
+# def parent_directory():
+#   # Create a relative path to the parent 
+#   # of the current working directory 
+#   relative_parent = os.path.join(os.getcwd(), "..")
 
-  # Return the absolute path of the parent directory
-  return os.path.realpath(relative_parent)
+#   # Return the absolute path of the parent directory
+#   return os.path.realpath(relative_parent)
 
-print(parent_directory())
+# print(parent_directory())
--- module 2/file.py
+++
@@ -1,7 +0,0 @@
-file = open("spider.txt")
-print(file.readline())
-print(file.readline())
-print(file.read())
-file.close()
-with open("spider.txt") as file:
-    print(file.readline())
